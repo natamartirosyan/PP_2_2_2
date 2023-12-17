21955632e82ec5f1facd630bae664fd17ff585c7
Natalya Martirosyan <nata.martirosyan@gmail.com>
2023-12-17 19:50:41 +0300

    second commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImpl;
-
-import java.util.List;
 
 @Controller
 public class CarController {
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -4,5 +4,5 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    public List<Car> getCarList(int count);
+    List<Car> getCarList(int count);
 }
